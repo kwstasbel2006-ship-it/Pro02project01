4c2b570ad6ce3964301d88ef063f21d7827f0e41
kwstasbel2006-ship-it <kwstabel2006@gmail.com>
2026-04-06 23:06:13 +0300

    stucture

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main (void){
-    printf("helow aleko");
+    printf("helow aleko");  //alekos 2,3,5
+                            //kvnstas 1,4
+
+    //while (){
+        
+    //}
     return 0;
 }
--- main2.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main (void){
-    printf("gamw to vs code");
-    return 0;
-}
