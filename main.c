f4f97bf23c83abaec86e1ac599a60f00a918f6d0
kwstasbel2006-ship-it <kwstabel2006@gmail.com>
2026-04-06 23:14:17 +0300

    dtrf

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 int main (void){
-<<<<<<< HEAD
-    printf("helow a leko");  //alekos 2,3,5
-=======
     printf("helow kyrie kosta");  //alekos 2,3,5
->>>>>>> 8e254e6d339b58f7a4dbbacf0f4dd34248028a16
                             //kvnstas 1,4
 
     //while (){
