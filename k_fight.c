f09df2c7cef18e85ea9f22ff3a2cc769ad991c59
kwstasbel2006-ship-it <kwstasbel2006@gmail.com>
2026-05-02 19:11:11 +0300

    soundthing

--- k_fight.c
+++ k_fight.c
@@ -147,37 +147,6 @@ void plasmagun(char **board ,char direction,char x){
     }
 }
 
-void soundThing(char **board, SoundDirection currentSound) {
-    printf("sound movement DOWN\n");
-    int i, j, target_row;
-
-    // Loop through every column independently
-    for (j = 0; j < cols; j++) {
-        
-        // Start target_row at the very bottom
-        target_row = rows - 1; 
-
-        // Scan upwards from the bottom
-        for (i = rows - 1; i >= 0; i--) {
-            
-            // If we hit a building, the 'floor' resets to just above the building!
-            if (board[i][j] == '#') {
-                target_row = i - 1; 
-            } 
-            // If we find a zombie, and it needs to fall...
-            else if (isZombie(board[i][j])) {
-                // Only move it if the target row is actually below it
-                if (target_row > i) {
-                    board[target_row][j] = board[i][j]; // Move it down
-                    board[i][j] = '.';                  // Clear old spot
-                }
-                // Once a zombie lands, the next available space is one row higher
-                target_row--; 
-            }
-        }
-    }
-}
-
 int isSameTypeZombie(char targetedZombie, char zombie){
     if (targetedZombie==zombie){
         return 1;
